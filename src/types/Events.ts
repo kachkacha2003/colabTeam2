17a40fe07e680ab125c9d82b5d8dc92407ed1095
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-10-16 23:00:00 +0400

    context fixed

--- src/types/Events.ts
+++ src/types/Events.ts
@@ -1,51 +1,43 @@
-export  interface IEvent {
-    id: number;
-    title: string;
-    description: string;
-    privacy: string;
-    medium: string;
-    startDate: string;
-    endDate: string;
-    duration: number;
-    language: string;
-    maxParticipants: number;
-    category: string;
-    terms: string;
-    LocationName: string;
-    latitude: number;
-    longtude: number;
-    acceptingRSVPs: string;
-    UploadImage: string;
-    Members: number;
-  };
- export type TEvents = IEvent[];
+export interface IEvent {
+  id: number;
+  title: string;
+  description: string;
+  privacy: string;
+  medium: string;
+  startDate: string;
+  endDate: string;
+  duration: number;
+  language: string;
+  maxParticipants: number;
+  category: string;
+  terms: string;
+  LocationName: string;
+  latitude: number;
+  longtude: number;
+  acceptingRSVPs: string;
+  UploadImage: string;
+  Members: number;
+}
+export type TEvents = IEvent[];
 
- export interface INewEventContext {
-   newEvent: TEvents;
-   setNewEvent:  React.Dispatch<React.SetStateAction<TEvents>>
-};
+export interface INewEventContext {
+  newEvent: TEvents;
+  setNewEvent: React.Dispatch<React.SetStateAction<TEvents>>;
+}
 
 export interface IUser {
-    id: number;
-    userName: string;
-    eMail: string;
-    photo: string;
-    };
+  id: number;
+  userName: string;
+  email: string;
+  photo: string;
+}
 export type TUsers = IUser[];
 export interface IUsers {
-   users: TUsers;
-   setUsers: React.Dispatch<React.SetStateAction<TUsers | undefined>>
+  users: TUsers;
+  setUsers: React.Dispatch<React.SetStateAction<TUsers | undefined>>;
 }
 
 export interface IShowMembers {
-   showMembers: boolean;
-   setShowMembers: React.Dispatch<React.SetStateAction<boolean>>
+  showMembers: boolean;
+  setShowMembers: React.Dispatch<React.SetStateAction<boolean>>;
 }
-
-
-
-
-
-
- 
-
