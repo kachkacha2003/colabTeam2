01fdf795c96e41c54795555b45cfed8284017c11
KatrinaZuus <katrinazuus@gmail.com>
2024-10-25 21:57:28 +0400

    map

--- src/Context/PageProvider.tsx
+++ src/Context/PageProvider.tsx
@@ -1,33 +1,41 @@
 import { createContext, useContext, ReactNode, useState } from "react";
-import { IEvent, IUser } from "../types/Events";
+import { IEvent, IUser, ICoordinates, INewEvent  } from "../types/Events";
 import { Itype } from "../types/ContextType";
 import data from "../data.json"
 
 
 const UsersContext = createContext<Itype>({
-  count: 0,
-  setCount: () => {},
   users: [],
-  setUsers: () => {},
+  setUsers: () => { },
   newEvent: [],
-  setNewEvent: () => {},
+  setNewEvent: () => { },
   showMembers: false,
-  setShowMembers: () => {}
-
+  setShowMembers: () => { },
+  coordinates: {
+    lat: 0,
+    log: 0
+  },
+  setCoordinates: () => {},
 });
 
 
 export default function UserProvider({ children }: { children: ReactNode }) {
-  const userData = data.data[3].users
+  const userData = data.data[3].users;
   const [users, setUsers] = useState<IUser[] | undefined>(userData);
-  const [count, setCount] = useState<number>(1);
-  const eventData = data.data[2].events
-  const [newEvent, setNewEvent] = useState<IEvent[] | undefined>(eventData)
+  const eventData = data.data[1].events;
+  const [newEvent, setNewEvent] = useState<IEvent[] | undefined>(eventData);
   const [showMembers, setShowMembers] = useState<boolean>(false)
+  const [coordinates, setCoordinates] = useState<ICoordinates | null>(null);
 
+console.log(newEvent)
 
   return (
-    <UsersContext.Provider value={{ count, setCount, users, setUsers, newEvent, setNewEvent, showMembers, setShowMembers }}>
+    <UsersContext.Provider value={{
+      users, setUsers, 
+      newEvent, setNewEvent, 
+      showMembers, setShowMembers, 
+      coordinates, setCoordinates,
+      }}>
       {children}
     </UsersContext.Provider>
   );
--- src/types/ContextType.ts
+++ src/types/ContextType.ts
@@ -1,13 +1,14 @@
-import { IUser, IEvent } from "./Events";
+import { SetStateAction } from "react";
+import { IUser, IEvent, ICoordinates } from "./Events";
 
 export interface Itype {
-  setCount: React.Dispatch<React.SetStateAction<number>>;
-  count: number;
   users: IUser[] | undefined;
   setUsers: React.Dispatch<React.SetStateAction<IUser[] | undefined>>;
   newEvent: IEvent[] | undefined;
-  setNewEvent: React.Dispatch<React.SetStateAction<IEvent[] | undefined>>;
+  setNewEvent: React.Dispatch<React.SetStateAction<IEvent [] | undefined>>;
   showMembers: boolean;
   setShowMembers: React.Dispatch<React.SetStateAction<boolean>>;
+  coordinates: ICoordinates | null;
+  setCoordinates: React.Dispatch<SetStateAction<ICoordinates | null>>
 }
 
